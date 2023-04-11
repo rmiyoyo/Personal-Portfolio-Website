895113fb36862b45398e938d3248932a0527c561
rmiyoyo <miyoyoraphael@gmail.com>
2023-04-11 18:06:18 +0300

    Implement the menu toggle feature for the mobile website.

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-// script.js
-document.addEventListener('DOMContentLoaded', function() {
-    const hamburgerIcon = document.querySelector('.svg-menu');
-    const mobileMenu = document.getElementById('nav-menu');
-  
-    hamburgerIcon.addEventListener('click', function() {
-      mobileMenu.classList.toggle('mobileMenu');
-    });
-  });
-  
+const handleClick = () => {
+  const mobileNavMenu = document.querySelector('#mobile-nav-menu');
+  mobileNavMenu.classList.toggle('mobile-menu-inactive');
+  mobileNavMenu.classList.toggle('mobile-menu');
+};
+
+document.addEventListener('DOMContentLoaded', () => {
+  document.querySelector('#svg-menu').addEventListener('click', handleClick);
+  document.querySelectorAll('.mobile-link, .disabled-button').forEach((button) => button.addEventListener('click', handleClick));
+});
