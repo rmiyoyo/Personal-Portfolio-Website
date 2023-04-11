6338219316be10797998069ba6f928843581ee02
rmiyoyo <miyoyoraphael@gmail.com>
2023-04-11 11:17:33 +0300

    Start adding JS functionalities

---
+++ script.js
@@ -0,0 +1,10 @@
+// script.js
+document.addEventListener('DOMContentLoaded', function() {
+    const hamburgerIcon = document.querySelector('.svg-menu');
+    const mobileMenu = document.getElementById('nav-menu');
+  
+    hamburgerIcon.addEventListener('click', function() {
+      mobileMenu.classList.toggle('show');
+    });
+  });
+  
