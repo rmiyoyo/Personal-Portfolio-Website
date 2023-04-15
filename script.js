74fba8842d01834d3b18eee66a0c0e070ceadc8c
rmiyoyo <miyoyoraphael@gmail.com>
2023-04-15 04:41:51 +0300

    Add contact form validation feature

--- script.js
+++ script.js
@@ -10,4 +10,24 @@ const handleClick = () => {
 document.addEventListener('DOMContentLoaded', () => {
   document.querySelector('#svg-menu').addEventListener('click', handleClick);
   document.querySelectorAll('.mobile-link, .disabled-button').forEach((button) => button.addEventListener('click', handleClick));
+
+  const form = document.querySelector('#contact-form');
+  const submitButton = document.querySelector('.contact-button');
+
+  form.addEventListener('submit', (event) => {
+    const emailInput = document.querySelector('.email-box');
+    const email = emailInput.value.trim();
+
+    if (email !== email.toLowerCase()) {
+      const errorMessage = document.createElement('p');
+      errorMessage.classList.add('error-message');
+      errorMessage.textContent = 'Email address must be in lowercase.';
+      const existingErrorMessage = submitButton.nextElementSibling;
+      if (existingErrorMessage && existingErrorMessage.classList.contains('error-message')) {
+        existingErrorMessage.remove();
+      }
+      submitButton.insertAdjacentElement('afterend', errorMessage);
+      event.preventDefault();
+    }
+  });
 });
