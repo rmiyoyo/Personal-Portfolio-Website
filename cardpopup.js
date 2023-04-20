8ae8cb2dd9c2923d2596f2cad66f4c3f157641d0
CELYNEBIRUNGI <113887926+CELYNEBIRUNGI@users.noreply.github.com>
2023-04-20 12:56:31 +0300

    rationalize javascript

--- cardpopup.js
+++ cardpopup.js
@@ -1,1 +1,4 @@
 const body = document.querySelector('.projectCardPop');
+const cards = document.getElementByID('projects');
+const popUp = document.createElement('section');
+const showButton = document.getElementsByClassName('extendTap');
