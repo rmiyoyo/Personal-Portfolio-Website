da7ed0125bdcfb7164aabc28d732f71890a550b0
rmiyoyo <miyoyoraphael@gmail.com>
2023-04-21 02:33:07 +0300

    Change technology stack in JS

--- cardpopup.js
+++ cardpopup.js
@@ -11,8 +11,8 @@ const popupInfo = [
     backend: 'Back End Dev',
     year: '2015',
     popupPhoto: './images/popup/popup_one.png',
-    content: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s,<br><br> when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent',
-    tools: ['HTML', 'CSS', 'JavaScript'],
+    content: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent',
+    tools: ['HTML', 'CSS', 'JavaScript', 'Ruby', 'Bootstrap'],
   },
 
   {
@@ -22,8 +22,8 @@ const popupInfo = [
     backend: 'Back End Dev',
     year: '2015',
     popupPhoto: './images/popup/popup_one.png',
-    content: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s,<br><br> when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent',
-    tools: ['HTML', 'CSS', 'JavaScript'],
+    content: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent',
+    tools: ['HTML', 'CSS', 'JavaScript', 'Ruby', 'Bootstrap'],
   },
 
   {
@@ -33,8 +33,8 @@ const popupInfo = [
     backend: 'Back End Dev',
     year: '2015',
     popupPhoto: './images/popup/popup_one.png',
-    content: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s,<br><br> when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent',
-    tools: ['HTML', 'CSS', 'JavaScript'],
+    content: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent',
+    tools: ['HTML', 'CSS', 'JavaScript', 'Ruby', 'Bootstrap'],
   },
 
   {
@@ -44,8 +44,8 @@ const popupInfo = [
     backend: 'Back End Dev',
     year: '2015',
     popupPhoto: './images/popup/popup_one.png',
-    content: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s,<br><br> when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent',
-    tools: ['HTML', 'CSS', 'JavaScript'],
+    content: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent',
+    tools: ['HTML', 'CSS', 'JavaScript', 'Ruby', 'Bootstrap'],
   },
 ];
 
